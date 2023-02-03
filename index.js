96ecf7277547349cc505f12add72b9ac7c8fe22a
fionaxu2023 <xuliefan@hotmail.com>
2023-02-03 16:21:30 -0500

    first update

---
+++ index.js
@@ -0,0 +1,31 @@
+//declare elements from html
+let count=document.getElementById("count");
+let hole=document.querySelectorAll(".hole")
+let hammer=document.getElementById("hammer")
+let score=0;
+let timer=null;
+
+//the function means to move the mole randomly and put it in the appropriate hole each time
+function moleMove(){
+    let i= Math.floor(Math.random() * hole.length);
+    let holechoose=hole[i];
+    //create image in the js
+    let imgmole= document.createElement("img");
+    imgmole.classList.add('mole')
+    imgmole.setAttribute("src","mole.png");
+
+    imgmole.addEventListener('click', function() {
+        console.log("click")
+        score += 1
+        count.textContent = score
+        holechoose.removeChild(imgmole)
+        moleMove;
+        })
+
+    holechoose.appendChild(imgmole);
+
+}
+
+
+
+moleMove()
